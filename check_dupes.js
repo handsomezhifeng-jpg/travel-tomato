d98a78d4a1134c4dacd32ae6fd8fb9d87a1dade6
zhfzhang <zhfzhang@yoozoo.com>
2026-03-19 17:39:33 +0800

    清理临时文件，更新 .gitignore

--- check_dupes.js
+++
@@ -1,6 +0,0 @@
-const d = require('./src/data/cities.json');
-const subset = d.slice(1000);
-const keys = subset.map(c => c.name + '_' + c.country);
-const dupes = keys.filter((k, i) => keys.indexOf(k) !== i);
-console.log('Total cities:', subset.length);
-console.log('Duplicate keys:', JSON.stringify(dupes));
--- tmp_check.js
+++
@@ -1,6 +0,0 @@
-const c = JSON.parse(require('fs').readFileSync('D:/lvyoufanqie/src/data/cities.json','utf8')).slice(0,1000);
-const keys = c.map(x => x.name + '_' + x.country);
-const dupes = keys.filter((k, i) => keys.indexOf(k) !== i);
-console.log('dupes:', dupes.length, dupes.slice(0, 10));
-console.log('unique keys:', new Set(keys).size);
-console.log('total:', c.length);
